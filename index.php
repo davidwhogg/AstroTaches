503d42968b7e66e2e61f4fc5fbd53722eee89540
Pamela Gay <pgay@dhcp-108-35.new.ox.ac.uk>
2011-04-05 17:42:14 +0100

    styles

--- index.php
+++ index.php
@@ -5,29 +5,49 @@ mysql_select_db('Summary', $db_sum);
 
 // Send the headers
 ?>
-<html><header>
+<html><head>
 	<link rel='stylesheet' 
           href='./style.css' 
           type='text/css'>
-</header>
 
+		<!--[if IE]><script src="excanvas.js"></script><![endif]-->
+		   <script src="jquery-1.4.2.js" type="text/javascript"></script>
+		   <script src="astrotaches.js" type="text/javascript"></script>
+		   <script>
+		   window.onload = function() {
+		       scribble = new AstroTaches({id:'AstroTaches'});
+		   }
+		   </script>
+</head>
+
+<body>
+	<div id="header">
+		Welcome to Astrotaches. Header goes here.<br>
+	</div>
+
+
+	<div id="sidebar">
+		This is a sidebar.
+		<?php
+		// Login
+		if (!isset($_COOKIE["stache"])) {
+			echo "<a href = './zoo-login.php'>Login</a>";
+		} else {
+			echo "<a href = './zoo-logout.php'>Logout</a>";
+		}
+		echo "<br>";
+		?>
+	</div>
+
+	<div id="main">
+		This is the Canvas<br>
+		<div id="AstroTaches">
+			<img src="examples/HSTJ135754_29-311509_1_sci.jpg">
+		</div>
+	</div>
+	
+	<div id="clear">
 <?php
-echo "Welcome to Astrotaches. Header goes here.<br>";
-
-// Sidebar
-echo "This is a sidebar.<br>";
-
-// Login
-if (!isset($_COOKIE["stache"])) {
-	echo "<a href = './zoo-login.php'>Login</a>";
-} else {
-	echo "<a href = './zoo-logout.php'>Logout</a>";
-}
-echo "<br>";
-
-// Canvas
-echo "This is the Canvas<br>";
-
 //Footer
 echo "This is the Footer<br>";
 
